09cecf0a6ba60efd3d348a6ca6d11e21ae69cc86
Mariusz Milewski <mar9mil@interia.pl>
2020-10-21 16:06:50 +0200

    wersja 1

---
+++ Kalkulator.py
@@ -0,0 +1,47 @@
+'''
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
+
+if __name__ == "__main__":
+    logging.debug("The program was called with this parameters %s" % sys.argv[1:])
+    logging.debug("First parameter is %s" % sys.argv[1])
+    age = int(sys.argv[1])
+    print_maturity(age)
+
+'''
+
+# Moduł 4
+# Zadanie 2
+
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+czy_kontynuowac = ""
+wynik = 0
+
+#while czy_kontynuowac == "n":
+
+dzialanie = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie :")
+składnik_pierwszy = input("Podaj składnik 1:")
+składnik_drugi = input("Podaj składnik 2:")
+
+if dzialanie == 1:
+    wynik = składnik_pierwszy + składnik_drugi
+    logging.info(f"dodaję {składnik_pierwszy} i {składnik_drugi}")
+    print(f"wynik to: {wynik}")
+elif dzialanie == 2:
+    wynik = składnik_pierwszy - składnik_drugi
+    logging.info(f"odejmuję {składnik_drugi} od {składnik_pierwszy}")
+    print(f"wynik to: {wynik}")
+elif dzialanie == 3:
+    wynik = składnik_pierwszy * składnik_drugi
+    logging.info(f"mnożę {składnik_pierwszy} i {składnik_drugi}")
+    print(f"wynik to: {wynik}")
+elif dzialanie == 4:
+    wynik = składnik_pierwszy / składnik_drugi
+    logging.info(f"dzielę {składnik_pierwszy} przez {składnik_drugi}")
+    print(f"wynik to: {wynik}")
+#else:
+#    print("Wybierz jedno z czterech działań")
+
+# czy_kontynuowac = input("Czy chcesz kontynuować liczenie (t/n):")
