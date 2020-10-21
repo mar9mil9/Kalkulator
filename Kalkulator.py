0bc368e14a6443e1e85e6811b2ce67ecf3dd42de
Mariusz Milewski <mar9mil@interia.pl>
2020-10-21 16:49:58 +0200

    poprawione typy zmiennych

--- Kalkulator.py
+++ Kalkulator.py
@@ -15,33 +15,34 @@ if __name__ == "__main__":
 # Zadanie 2
 
 import logging
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 czy_kontynuowac = ""
 wynik = 0
 
 #while czy_kontynuowac == "n":
 
 dzialanie = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie :")
-składnik_pierwszy = input("Podaj składnik 1:")
-składnik_drugi = input("Podaj składnik 2:")
+składnik_pierwszy = int(input("Podaj składnik 1:"))
+składnik_drugi = int(input("Podaj składnik 2:"))
 
-if dzialanie == 1:
+if dzialanie == "1":
     wynik = składnik_pierwszy + składnik_drugi
     logging.info(f"dodaję {składnik_pierwszy} i {składnik_drugi}")
     print(f"wynik to: {wynik}")
-elif dzialanie == 2:
+elif dzialanie == "2":
     wynik = składnik_pierwszy - składnik_drugi
     logging.info(f"odejmuję {składnik_drugi} od {składnik_pierwszy}")
     print(f"wynik to: {wynik}")
-elif dzialanie == 3:
+elif dzialanie == "3":
     wynik = składnik_pierwszy * składnik_drugi
     logging.info(f"mnożę {składnik_pierwszy} i {składnik_drugi}")
     print(f"wynik to: {wynik}")
-elif dzialanie == 4:
+elif dzialanie == "4":
     wynik = składnik_pierwszy / składnik_drugi
     logging.info(f"dzielę {składnik_pierwszy} przez {składnik_drugi}")
     print(f"wynik to: {wynik}")
-#else:
-#    print("Wybierz jedno z czterech działań")
+else:
+    print("Wybierz jedno z czterech działań")
+    logging.info(f"podałeś : {dzialanie}")
 
 # czy_kontynuowac = input("Czy chcesz kontynuować liczenie (t/n):")
